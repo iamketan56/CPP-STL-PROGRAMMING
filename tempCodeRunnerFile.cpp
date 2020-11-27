4c9aabf085f1239c7e8226e4556284d037ce37b7
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-27 06:45:21 +0530

    Create tempCodeRunnerFile.cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+void print(std::stack<int> stk)
+{
+    while (!stk.empty())
+    {
+        cout << stk.top() << endl;
+        stk.pop();
+    }
+}
+int main()
+{
+    std::stack<int> Stack;
+    Stack.push(1);
+    Stack.push(2);
+    Stack.push(3);
+    Stack.push(4);
+    print(Stack);
+}
