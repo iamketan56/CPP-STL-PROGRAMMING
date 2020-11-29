55e15599eb0e22efae9bdac2e90e0ff9fae3ff35
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-29 07:26:06 +0530

    Create Operation_On_ForwardList.cpp

---
+++ Operation_On_ForwardList.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <forward_list>
+using namespace std;
+int main()
+{
+    std::forward_list<int> fl;
+    //assign
+    fl.assign({2, 3, 4, 5, 6});
+    for (auto &el : fl)
+    {
+        cout << el << " ";
+    }
+    cout << endl;
+    //push front
+    fl.push_front(1);
+    for (auto &el : fl)
+    {
+        cout << el << " ";
+    }
+    cout << endl;
+    //front
+    cout << fl.front();
+    cout << endl;
+
+    std::forward_list<int> fl1;
+    fl1.assign({11, 12, 13, 14, 15});
+
+    fl.splice_after(fl.begin(), fl1);
+    for (auto &el : fl)
+    {
+        cout << el << " ";
+    }
+    cout << endl;
+    // pop_front
+    fl.pop_front();
+
+    for (auto &el : fl)
+    {
+        cout << el << " ";
+    }
+    cout << endl;
+    //remove
+    fl.remove(4);
+    //remove if
+    fl.remove_if([](int x) { return x > 2; });
+    for (auto &el : fl)
+    {
+        cout << el << " ";
+    }
+}
