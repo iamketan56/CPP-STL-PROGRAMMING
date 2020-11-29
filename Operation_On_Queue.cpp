9d277bbfdb8065bcad4343ab0f8f0fae1bf8334f
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-11-29 07:58:40 +0530

    Update Operation_On_Queue.cpp

--- Operation_On_Queue.cpp
+++ Operation_On_Queue.cpp
@@ -5,21 +5,30 @@ void print(std::queue<int> q)
 {
     while (!q.empty())
     {
-        cout << q.front() << endl;
+        cout << q.front() << " ";
         q.pop();
     }
 }
 int main()
 {
     std::queue<int> que;
+    std::queue<int> que1;
+    que1.push(3);
+    que1.push(5);
+    que1.push(7);
+    que1.push(9);
+    que1.push(11);
     que.push(1);
     que.push(2);
     que.push(3);
     que.push(4);
     que.pop();
     que.push(5);
-    print(que);
+    // print(que);
     cout << que.size();
     cout << endl;
     cout << que.front();
+    cout << endl;
+    que.swap(que1);
+    print(que);
 }
